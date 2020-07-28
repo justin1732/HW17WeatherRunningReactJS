0baefa323c152699a6eef107108e4a33adab9149
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-07-27 22:59:40 -0500

    Setting up the React App

---
+++ weather-running-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1>React Weather App</h1>
+      </header>
+      <main>
+      </main>
+      <footer>
+        Page created by Justin Tucker, 2020
+      </footer>
+    </div>
+  );
+}
+
+export default App;
